16e30d11b4c170c82c2c5f3f871dd0095c8c1e04
Joby Ingram-Dodd <joby@protonmail.com>
2021-01-26 10:25:58 +0000

    tests

--- knn.py
+++ knn.py
@@ -6,10 +6,16 @@ class Knn:
         self.k = k
 
     def fit(self, X, y):
-        return "this model is to lazy to fit, just go right to prediction"
+        print("this model is to lazy to fit, just go right to prediction")
+        return self
 
     def check_for_missing_values(self, test, train, y_train):
-        pass
+        tc = np.count_nonzero(np.isnan(test))
+        trc = np.count_nonzero(np.isnan(train))
+        ytc = np.count_nonzero(np.isnan(y_train))
+        if tc > 0 or trc > 0 or ytc > 0:
+            raise Exception("Data must not contain nan values")
+
 
     def find_neighbours(self,x, data, d_class):
         # euc distance for all data points
@@ -17,30 +23,23 @@ class Knn:
         self.neighbours = {}
         for d in data:
             dist = np.linalg.norm(d-x)
-            print(dist)
             index = np.where(data == d)
-            #print(index, index[0][0])
-            print("class should be: ", d_class[index[0][0]])
             self.neighbours[dist] = d_class[index[0][0]]
-        #print(self.neighbours)
+
 
     def vote(self):
         keys = np.array(sorted(self.neighbours.keys()))
         # get the k number of lowest value
         cla = keys[:self.k]
-        #print(cla)
         options = []
         for c in cla:
             options.append(self.neighbours[c])
         unique, frequency = np.unique(options, return_counts = True)
         index = np.where(frequency == max(frequency))
-        #print(unique, frequency, index[0])#frequency.index(max(frequency))
-        print("Unique is ", unique)
-        print("The Class is ", unique[index[0]])
         self.classifcation.append(unique[index[0]][0])
-        #print("classifaction is ",self.classifcation)
 
     def predict(self,t,X,y):
+        self.check_for_missing_values(t,X,y)
         self.classifcation = []
         for x in t:
             self.find_neighbours(x,X,y)
--- test_accuracy.py
+++ test_accuracy.py
@@ -5,4 +5,10 @@ knn = knn.Knn(5)
 X_train,X_test, y_train, y_test = tt.smaple_data()
 
 print("Length of the test set: ",len(X_test))
-print(knn.predict(X_test,X_train,y_train))
+preds = knn.predict(X_test,X_train,y_train)
+score = 0
+for i in range(len(preds)):
+    if y_test[i] == preds[i]:
+        score += 1
+accuracy = score / len(y_test)
+print("accuracy of: ",accuracy)
