d59ae63c6dd0d651cded153feb05a807e951baf6
Joby Ingram-Dodd <joby@protonmail.com>
2021-01-26 13:44:47 +0000

    tests

--- knn.py
+++ knn.py
@@ -1,9 +1,11 @@
 import numpy as np
-import test as tt
+import time
 
 class Knn:
     def __init__(self, k):
         self.k = k
+        self.start_time = time.time()
+        self.execution_time = 0.00
 
     def fit(self, X, y):
         print("this model is to lazy to fit, just go right to prediction")
@@ -39,13 +41,24 @@ class Knn:
         self.classifcation.append(unique[index[0]][0])
 
     def predict(self,t,X,y):
+        self.execution_time_set()
         self.check_for_missing_values(t,X,y)
         self.classifcation = []
         for x in t:
             self.find_neighbours(x,X,y)
             self.vote()
-        print(len(self.classifcation))
+        self.execution_time = time.time() - self.start_time
         return self.classifcation
 
+    def execution_time_set(self):
+        self.start_time = time.time()
+
+    def evaluate(self,true):
+        score = 0
+        for i in range(len(self.classifcation)):
+            if true[i] == self.classifcation[i]:
+                score += 1
+        accuracy = score / len(true) * 100
+        return accuracy
 # square root of n sometimes a good option for k and should be odd
 
